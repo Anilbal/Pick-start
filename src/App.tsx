a393487d599722dd2e0709d7fb18f18abb02c35e
Anilbal <anilbal7676@gmail.com>
2025-12-09 01:15:22 +0545

    re beginning

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import Stepper from "./components/elements/stepper/stepper.tsx";
-
-const steps = [
-    {name: "User", component: () => <h1>This is one</h1>},
-    {name: "Language", component: () => <h1>This is two</h1>},
-    {name: "Frameworks", component: () => <h1>This is three</h1>},
-    {name: "Overview", component: () => <h1>This is four</h1>},
-];
-
 function App() {
-    return (
-        <div>
-            <Stepper steps={steps}/>
-        </div>
-    );
+  return <div> hello </div>;
 }
 
 export default App;
