d59db59ac55a24cc02334aa60c4eca8963ae8031
Anil Bal <anilbal7676@gmail.com>
2025-12-13 00:47:44 +0545

    fix

--- src/components/content/nodeWithPostgres/NodeWithPostgres.tsx
+++ src/components/content/nodeWithPostgres/NodeWithPostgres.tsx
@@ -5,7 +5,7 @@ const NodeWithPostgres: React.FC = () => {
   return (
     <div className="p-6">
       <h1 className="text-xl font-medium underline">Overview:</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
         {BackendConstants.map((item,index)=>(
             <Card index={`1.${index+1}`} title={item} path="" />
         ))}
