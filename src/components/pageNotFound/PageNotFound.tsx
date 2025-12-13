a3e6ac444efb9b0e981d72dcba7d950bff41969d
Anil Bal <anilbal7676@gmail.com>
2025-12-13 16:29:35 +0545

    back to previous in not found page

--- src/components/pageNotFound/PageNotFound.tsx
+++ src/components/pageNotFound/PageNotFound.tsx
@@ -1,6 +1,7 @@
-import { Link } from "@tanstack/react-router";
+import { useRouter } from "@tanstack/react-router";
 
 const NotFound: React.FC = () => {
+  const router = useRouter();
   return (
     <div className="flex h-[calc(100vh-56px)] flex-col items-center justify-center bg-gray-50 px-6 text-center">
       <h1 className="text-9xl font-extrabold text-gray-700">404</h1>
@@ -13,12 +14,12 @@ const NotFound: React.FC = () => {
         The page you're looking for doesn’t exist or has been moved.
       </p>
 
-      <Link
-        to="/home"
+      <button
+        onClick={() => router.history.back()}
         className="mt-6 rounded-md bg-blue-600 px-6 py-3 text-white font-medium transition hover:bg-blue-700"
       >
         Go Home
-      </Link>
+      </button>
     </div>
   );
 };
